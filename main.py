41788e8de8889005922eb95016cb6334593fc41e
Troy M <xapd421@gmail.com>
2024-04-21 00:33:00 -0700

    finish dice sim

---
+++ main.py
@@ -0,0 +1,47 @@
+from random import randint
+
+def rollDice(times):
+    rolls = []
+    for i in range(times):
+        rolls.append(randint(1, 6))
+    return rolls
+
+def getStats(rolls):
+    stats = {
+        "ones": 0,
+        "twos": 0,
+        "threes": 0,
+        "fours": 0,
+        "fives": 0,
+        "sixes": 0
+    }
+
+    for i in range(len(rolls)):
+        if rolls[i] == 1:
+            stats["ones"] += 1
+        elif rolls[i] == 2:
+            stats["twos"] += 1
+        elif rolls[i] == 3:
+            stats["threes"] += 1
+        elif rolls[i] == 4:
+            stats["fours"] += 1
+        elif rolls[i] == 5:
+            stats["fives"] += 1
+        elif rolls[i] == 6:
+            stats["sixes"] += 1
+
+    return stats
+
+# rolls = rollDice(10)
+# print(rolls)
+# stats = getStats(rolls)
+# print(stats)
+
+print("Welcome to my create task")
+print("This app simulates rolling a dice")
+while True:
+    times = int(input("How many times will you roll the dice? "))
+    rolls = rollDice(times)
+    stats = getStats(rolls)
+    print(f"Rolls: {rolls}")
+    print(f"Stats: {stats}")
